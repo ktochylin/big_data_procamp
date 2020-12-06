73e574a4c0613c84b89344437aa1fcd0eeb958dc
ktochylin <alivegbox@gmail.com>
2020-12-06 20:37:37 +0200

    Labs kafka homework (#4)
    * first test_consumer_001.py
    
    * synchronized commit added
    
    * line endings and requirements.txt
    
    * new topic bitstamp.transaction.btcusd
    
    * analyse only sell orders
    
    * updates
    
    * update
    
    * update
    
    * update
    
    * update
    
    * rename program, add doc files
    
    * update
    
    * update
    
    * update
    
    * update
    
    Co-authored-by: Kostiantyn Tochylin <kostiantyn.tochylin@gettyimages.com>

---
+++ labs/kafka/README.md
@@ -0,0 +1,74 @@
+# Apache Kafka Lab 1
+
+## My changes: it shows Top 10 out of 100 messages with Top 10 updated as in description
+
+# Solution
+
+## NiFi part
+
+
+### Deploy NiFi ingestion process manually
+### and start workflow before run Kafka application
+
+\big_data_procamp\labs\kafka\homework\nifi\bitstamp_kafka_publisher.xml
+
+### Do not forget these settings
+
+Property            | Value
+--------------------|-------------------------------
+Truststore Filename | /opt/gl-bigdata-procamp/bitstamp.truststore
+Truststore Password | truststore
+Truststore Type     | JKS
+
+Name         | Value
+-------------|-------------------------------
+CurrencyPair | btcusd
+
+### After deployment run this process
+
+### NiFi workflow
+![](images/001.PNG)
+
+## Kafka part
+### below instructions I run on master node form logged in user folder
+
+```
+# Create Kafka Topic
+/usr/lib/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 1 --topic bitstamp.transaction.btcusd
+
+# Verify Kafka Topic
+/usr/lib/kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181
+
+# Clone GIT project with application
+git clone https://github.com/ktochylin/big_data_procamp.git
+
+# Go to GIT repository
+cd ~/big_data_procamp
+
+# Verify we are in a main branch
+git branch
+
+git pull
+
+# Create python 3 virtual environment
+mkdir venv
+
+virtualenv --python=python3 venv
+
+# Activate virtualenv
+source venv/bin/activate
+
+# Install proprietary Kafka library
+pip install confluent-kafka
+
+# Go to application folder
+# cd ~/big_data_procamp/labs/kafka/homework/
+cd labs/kafka/homework
+
+# Run application
+python show_top10_transactions_for_sale.py
+
+```
+
+### Kafka application output
+![](images/002.PNG)
---
+++ labs/kafka/homework/show_top10_transactions_for_sale.py
@@ -0,0 +1,74 @@
+from confluent_kafka import Consumer
+import json
+from time import sleep
+
+conf = {'bootstrap.servers': 'localhost:9092',
+		'group.id': 'mygroup',
+		#'auto.offset.reset': 'latest',
+		'default.topic.config': {'auto.offset.reset': 'earliest'},
+		'enable.auto.commit': 'false'}
+		
+consumer = Consumer(conf)
+# Set of params could be passed as console arguments, please change if need
+topics = ["bitstamp.transaction.btcusd"]
+top_x_count = 10
+top_batch_count = 100
+
+def consume_loop(consumer, topics, top_x, commit_count):
+	try:
+		consumer.subscribe(topics)
+		transaction_list = []
+		msgno = 0
+		while True:
+			msg = consumer.poll(1.0)
+			if msg is None:
+				continue
+			if msg.error():
+				print("Consumer error: {}".format(msg.error()))
+				continue
+			else:
+				# Deserialize kafka message
+				data = json.loads(msg.value().decode('utf-8'))["data"]
+				if data == {}:
+					print("This is Subscription message")
+					print(data, "\n")
+					continue
+				# Collect sale transactions
+				if data["order_type"] == 1:
+					transaction_list.append(data)
+					msgno += 1
+					# Do batch
+					if msgno % commit_count == 0:
+						print("commit_count + top{} = {}: ".format(top_x, len(transaction_list)), "\n")
+						print("Top 10 transactions by Price", "\n")
+						# Get Top X
+						transaction_list = sorted(transaction_list, key = lambda i: i['price'], reverse=True)[:top_x]
+						# Header
+						print(" {: >15} | {: >4} | {: >16} | {}".format("transaction id", "type", "price", "amount"))
+						# Top X
+						for transaction in transaction_list:
+							print("{} | {: >4} | {: >16f} | {: f}".format( \
+									transaction["id"], \
+									transaction["order_type"], \
+									transaction["price"], \
+									transaction["amount"]))
+						
+						print("\n\n")
+						# at least once (Commit after show Top X)
+						# print("Commit info nessage:")
+						consumer.commit(asynchronous=False)
+						
+						sleep(1)
+	except KeyboardInterrupt:
+		print("Press Ctrl-C to terminate while statement")
+		pass
+	# If not expected error
+	except Exception as ex:
+		print("Something happened", ex.with_traceback())
+		raise
+	finally:
+		# Close down consumer to commit final offsets.
+		consumer.close()
+
+if __name__ == "__main__":
+	consume_loop(consumer, topics, top_x_count, top_batch_count)
